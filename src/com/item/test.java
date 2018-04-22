e1c2d6cb96df300181cfc32eb8b3f46b3818580c
shidiwen <qq278725160>
2018-04-22 23:42:51 +0800

    测试提交

--- src/com/item/test.java
+++ src/com/item/test.java
@@ -2,7 +2,7 @@ package com.item;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("第三次修改2了");
+        System.out.println("第三次修改22了");
         System.out.println("呵呵我来了");
         System.out.printf("hello");
     }
